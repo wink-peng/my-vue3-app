1c3c4c79b56a21eb0a1e70749ea7b8dbc00fecfa
pengyoushu <pengyoushu@zbj.com>
2025-05-07 18:26:49 +0800

    feat:双向绑定

---
+++ src/components/Test.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>TEST</div>
+    <input :value="modelValue" @input="handleInput"/>
+    <div style="display:flex;backgroundColor:#ff6900;justify-content:space-between">
+        <div>1111</div>
+        <div>2222</div>
+        <div>3333</div>
+        <div>4444</div>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+defineProps({
+    modelValue:{
+        type:String,
+        required:true
+    }
+})
+const emit = defineEmits(['update:modelValue'])
+const handleInput = (e)=>{
+    // 输出事件对象
+    console.log(e)
+    // 注释掉的事件触发，用于更新绑定的模型值
+    emit('update:modelValue',e.target.value)
+}
+</script>
+<style lang="less" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
-    <h1>首页</h1>
+    <h1></h1>
+    <button>Load Posts</button>
+    <button >Creste Test Post</button>
+    <div class="loading">loading...</div>
     <p>欢迎来到我的网站</p>
+    {{data2}}
+    <p>===data2会随着子组件的变化而变化，因为实现了双向绑定</p>
+    <Test v-model='data2'/>
   </div>
 </template>
+<script setup>
+import Test from '@/components/Test.vue'
+import { ref, watchEffect } from 'vue'
+const data2 = ref('')
+watchEffect(()=>{
+  console.log(data2.value)
+})
+</script>
+<style lang="less" scoped>
+
+</style>
